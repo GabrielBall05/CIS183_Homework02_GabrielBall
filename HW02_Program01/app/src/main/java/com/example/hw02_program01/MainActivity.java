501a37bb11b73af660a816be791739e8a65031f3
GabrielBall05 <gabrielb4556@gmail.com>
2023-09-21 13:40:12 -0400

    GUI Established - Button Works
    Made GUI design/built connection on Java side. Button Press works

--- HW02_Program01/app/src/main/java/com/example/hw02_program01/MainActivity.java
+++ HW02_Program01/app/src/main/java/com/example/hw02_program01/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.hw02_program01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    TextView tv_j_redVal;
+    TextView tv_j_greenVal;
+    TextView tv_j_blueVal;
+    TextView tv_j_hex;
+    SeekBar sb_j_red;
+    SeekBar sb_j_green;
+    SeekBar sb_j_blue;
+    Button btn_j_saveColor;
+    ListView lv_j_colors;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_j_redVal = findViewById(R.id.tv_v_redVal);
+        tv_j_greenVal = findViewById(R.id.tv_v_greenVal);
+        tv_j_blueVal = findViewById(R.id.tv_v_blueVal);
+        tv_j_hex = findViewById(R.id.tv_v_hex);
+        sb_j_red = findViewById(R.id.sb_v_red);
+        sb_j_green = findViewById(R.id.sb_v_green);
+        sb_j_blue = findViewById(R.id.sb_v_blue);
+        btn_j_saveColor = findViewById(R.id.btn_v_saveColor);
+
+
+        SaveColorOnClickListener();
     }
+
+    public void SaveColorOnClickListener()
+    {
+        btn_j_saveColor.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Log.d("Button Press", "Save Color Button Pressed");
+
+
+            }
+        });
+    }
+
+
+
 }
